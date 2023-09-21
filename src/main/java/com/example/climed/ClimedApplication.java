c410535857a63e3a4a49e9d70d37bf676779430f
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-09-21 15:06:49 -0300

    Initial commit

---
+++ src/main/java/com/example/climed/ClimedApplication.java
@@ -0,0 +1,13 @@
+package com.example.climed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/climed/ClimedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
